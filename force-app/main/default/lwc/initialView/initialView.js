54cc1bc6b1189f044c70949f843fa01046a20a5c
Tanis Pan <e.pan.gar@gmail.com>
2021-12-15 22:25:45 +0100

    Front-Back configuration

--- force-app/main/default/lwc/initialView/initialView.js
+++ force-app/main/default/lwc/initialView/initialView.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,  api, wire, track } from 'lwc';
+import getTitle from '@salesforce/apex/InitialClass.getTitle';
+import getAccounts from '@salesforce/apex/InitialClass.getAccounts';
 
-export default class InitialView extends LightningElement {}
+
+export default class InitialView extends LightningElement {
+  @track title;
+  @track accounts;
+  @track error;
+  
+  connectedCallback() {
+    getTitle()
+        .then(result => {
+            this.title = result;
+        })
+        .catch(error => {
+            this.error = error;
+        });
+    
+    getAccounts()
+        .then(result => {
+          this.accounts = result;
+        })
+        .catch(error => {
+            this.error = error;
+        });
+
+  }
+  
+}
